5aa2e54175f106e1909cc735fd8bd5e1304749ea
Michael Klishin <michael@defprotocol.org>
2013-01-30 19:23:26 +0400

    1.1.0-beta1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/spyglass "1.1.0-SNAPSHOT"
+(defproject clojurewerkz/spyglass "1.1.0-beta1"
   :description "A Clojure client for Memcached implemented as a very thin layer on top of SpyMemcached"
   :url "http://github.com/clojurewerkz/spyglass"
   :license {:name "Eclipse Public License"
