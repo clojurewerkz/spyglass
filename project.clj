ac36b78bfab9862e692a1ed86bd4eebb5424f05b
Michael Klishin <michael@clojurewerkz.org>
2014-03-27 09:33:06 +0400

    Master is now 1.7 snapshot

--- project.clj
+++ project.clj
@@ -18,8 +18,8 @@
   :javac-options     ["-target" "1.6" "-source" "1.6"]  
   :warn-on-reflection true
   :profiles       {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-                   :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
-                   :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+                   :1.6 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
+                   :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
                    :dev {:resource-paths ["test/resources"]
                          :dependencies [[org.clojure/core.cache "0.6.2" :exclusions [org.clojure/clojure]]]
                          :plugins [[codox "0.6.6"]]
