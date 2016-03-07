98b19fdb66ea4014532a16c5f10573ede06c8b09
Timo Sulg <timgluz@gmail.com>
2016-03-07 21:38:44 +0100

    add experimental nippy transcoder;

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [net.spy/spymemcached "2.12.0"]
-                 [com.couchbase.client/java-client "2.2.4"]]
+                 [com.couchbase.client/java-client "2.2.4"]
+                 [com.taoensso/nippy "2.11.1"]]
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
@@ -31,3 +32,4 @@
                    :focus         :focus
                    :couchbase     :couchbase
                    :all           (constantly true)})
+
--- src/clojure/clojurewerkz/spyglass/transcoder.clj
+++ src/clojure/clojurewerkz/spyglass/transcoder.clj
@@ -1,6 +1,7 @@
 (ns clojurewerkz.spyglass.transcoder
   "Transcoder is an interface for classes that convert between byte arrays
   and objects for storage in the cache."
+  (:require [taoensso.nippy :as nippy])
   (:import [net.spy.memcached CachedData]
            [net.spy.memcached.transcoders
               Transcoder IntegerTranscoder LongTranscoder
@@ -19,3 +20,35 @@
 
 (defmethod make-transcoder :serializing [_]
   (SerializingTranscoder.))
+
+
+;;experimental Nippy transcoder
+(deftype NippyTranscoder []
+  net.spy.memcached.transcoders.Transcoder
+  (asyncDecode [this dt] false)
+  (getMaxSize [this]
+    (int CachedData/MAX_SIZE))
+
+  (decode [this dt]
+    (-> dt (.getData) nippy/thaw))
+
+  (encode [this clj-obj]
+    (CachedData. (int 0)
+                 (nippy/freeze clj-obj)
+                 CachedData/MAX_SIZE)))
+
+(defmethod make-transcoder :nippy [_]
+  (NippyTranscoder.))
+
+(comment
+  (require '[clojurewerkz.spyglass.client :as c])
+  (require '[clojurewerkz.spyglass.transcoder :as t] :reload)
+  (def tc (c/text-connection "192.168.99.100:11211"))
+  
+  (def nippler (t/make-transcoder :nippy))
+  @(c/set tc "abc" 30 (long 123) nippler)
+  (c/get tc "abc" nippler)
+
+
+  )
+
