6b80e8a0579e33b98095573f6b6e78124508eb22
Michael Klishin <michael@clojurewerkz.org>
2017-06-21 22:51:50 +0300

    Depend on Clojure 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/clojurewerkz/spyglass"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.spy/spymemcached "2.12.0"]
                  [com.couchbase.client/java-client "2.2.4"]]
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
@@ -17,15 +17,14 @@
   :javac-options     ["-target" "1.8" "-source" "1.8"]
   :global-vars {*warn-on-reflection* true
                 *assert* false}
-  :profiles       {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-                   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-                   :master {:dependencies [[org.clojure/clojure "1.8.0"]]}
+  :profiles       {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+                   :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
                    :dev {:resource-paths ["test/resources"]
                          :dependencies [[org.clojure/core.cache "0.6.4" :exclusions [org.clojure/clojure]]]
                          :plugins [[codox "0.8.10"]]
                          :codox {:sources ["src/clojure"]
                                  :output-dir "doc/api"}}}
-  :aliases        {"all" ["with-profile" "dev:dev,1.5:dev,1.7:dev,master"]}
+  :aliases        {"all" ["with-profile" "dev:dev,1.7:dev,master"]}
   :test-selectors {:default     (fn [m]
                                   (and (not (:couchbase m))))
                    :focus         :focus
